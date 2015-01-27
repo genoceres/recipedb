bcf2d85174fbc31e2404ae2aa7ee485b2ffdbfac
genoceres <alec.horley@oceres.com>
2015-01-27 21:57:56 +0000

    added doctrine and first model (basic)

---
+++ src/Oceres/RecipeBundle/Controller/RecipeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oceres\RecipeBundle\Controller;
+
+use Oceres\RecipeBundle\Entity\Recipe;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RecipeController extends Controller
+{
+    public function recipeAction($name,$recipeid)
+    {
+    
+    	$recipe = $this->getDoctrine()->getRepository('OceresRecipeBundle:Recipe')->find($recipeid);
+    	
+
+    	$params = array('recipe' => $recipe);
+        return $this->render('OceresRecipeBundle:Frontend:recipe.html.twig',$params);
+    }
+
+    public function helloAction($name)
+    {
+        return $this->render('OceresRecipeBundle:Default:hello.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Oceres/RecipeBundle/Entity/Recipe.php
@@ -0,0 +1,106 @@
+<?php
+namespace Oceres\RecipeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="recipes")
+ */
+class Recipe {
+	
+	
+	/**
+	 * @ORM\Column(type="integer", name="rp_id")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	
+	/**
+	 * @ORM\Column(type="string", length=100, name="rp_title")
+	 */
+	protected $title;
+	
+	/**
+	 * @ORM\Column(type="string", length=100, name="rp_chef")
+	 */
+	protected $chef;
+	
+	/**
+	 * @ORM\Column(type="string", length=20, name="rp_course")
+	 */
+	protected $course;
+	
+	/**
+	 * @ORM\Column(type="integer", name="rp_serves")
+	 */
+	protected $serves;
+	
+	/**
+	 * @ORM\Column(type="string", length=4000, name="rp_ingredients")
+	 */
+	protected $ingredients;
+	
+	/**
+	 * @ORM\Column(type="string", length=4000, name="rp_method")
+	 */
+	protected $method;
+	
+	/**
+	 * @ORM\Column(type="string", length=20, name="rp_image")
+	 */
+	protected $image;
+	
+	/**
+	 * @ORM\Column(type="string", length=250, name="rp_tags")
+	 */
+	protected $tags;
+	
+	/**
+	 * @ORM\Column(type="datetime", name="rp_indt")
+	 */
+	protected $indt;
+	
+	/**
+	 * @ORM\Column(type="datetime", name="rp_moddt")
+	 */
+	protected $moddt;
+	
+	/**
+	 * @ORM\Column(type="smallint", name="rp_user")
+	 */
+	protected $user;
+	
+	/**
+	 * @ORM\Column(type="datetime", name="rp_lastaccessed")
+	 */
+	protected $lastaccessed;
+	
+	public function __get($param){
+		
+		switch($param){
+			case 'ingredients':
+				$out = explode("\n",$this->ingredients);
+				break;
+			case 'method':
+				$out = explode("\n",$this->method);
+				break;
+			case 'indt':
+			case 'moddt':
+			case 'lastaccessed':
+				return $this->$param->format('D jS \o\f M, Y');
+				break;
+			default:
+				$out = isset($this->$param) ? $this->$param : '';
+		}
+		
+		return $out;
+		
+	}
+	
+	public function __isset($param){
+		return property_exists($this,$param);
+	}
+	
+}
